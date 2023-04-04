9ef4a8b073b4730ac9cfa5050874cb1b97d0c5b2
Buster <otis1017@gmail.com>
2023-04-03 22:49:38 -0400

    asdf

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -691,7 +691,7 @@ const Chat = () => {
         >
           <div className="text-muted d-flex justify-content-center align-items-center pt-3">
             <Image
-              src={userAvatarImg}
+              src={userAvatar}
               onClick={() => {setShowAvatarModal(true)}}
               roundedCircle
               style={{
