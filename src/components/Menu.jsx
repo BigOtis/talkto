75fd730a17f4c73ac4cf50a1f39f2a71705dfcd3
Buster <otis1017@gmail.com>
2023-04-04 22:30:17 -0400

    update header

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -17,7 +17,7 @@ const Menu = () => {
             className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
           >
             <Image src={otisfuse} alt="" rounded className="img-fluid me-2" style={{ width: "50px", height: "50px" }} />
-            <span style={{ fontSize: "1.5rem" }}>Talk To AI</span>
+            <span style={{ fontSize: "1.5rem" }}>Chat With AI</span>
           </Link>
           <Nav>
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
