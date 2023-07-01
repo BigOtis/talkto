c264b7782364c2ea42715cb54c033708af68c726
Buster <otis1017@gmail.com>
2023-07-01 13:35:35 -0400

    donate button

--- src/components/Chat/DonationModal.jsx
+++ src/components/Chat/DonationModal.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Modal, Button, Form, FormCheck } from 'react-bootstrap';
 
 const DonationsModal = ({ showDonationModal, setShowDonationModal }) => {
+  const [doNotShowAgain, setDoNotShowAgain] = useState(false);
+
+  useEffect(() => {
+    const value = localStorage.getItem('donationModalDoNotShowAgain');
+    setDoNotShowAgain(value === 'true');
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('donationModalDoNotShowAgain', doNotShowAgain);
+  }, [doNotShowAgain]);
+
+  const handleClose = () => {
+    setShowDonationModal(false);
+  };
+
+  if (doNotShowAgain) {
+    return null;
+  }
+
   return (
-    <Modal show={showDonationModal} onHide={() => setShowDonationModal(false)}>
+    <Modal show={showDonationModal} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Support OtisFuse Chat!</Modal.Title>
       </Modal.Header>
@@ -14,12 +33,13 @@ const DonationsModal = ({ showDonationModal, setShowDonationModal }) => {
           <li>Provide a fun, ad-free service</li>
           <li>Maintain and improve the site</li>
         </ul>
-        <blockquote className="blockquote">
-          <p><i>"I love using OtisFuse chat. It's helped me stay connected with my friends and family. I happily support them!"</i></p>
-          <footer className="blockquote-footer">Happy User</footer>
-        </blockquote>
         <h5>Suggested donation amounts: $5, $10, $20, or choose what you can give. Every bit helps!</h5>
-        <Form.Check type="checkbox" label="Set up a monthly donation" />
+        <FormCheck type="checkbox" label="Set up a monthly donation" />
+        <FormCheck 
+          type="checkbox" 
+          label="I donated! Please don't show me this again" 
+          checked={doNotShowAgain} 
+          onChange={e => setDoNotShowAgain(e.target.checked)} />
       </Modal.Body>
       <Modal.Footer>
         <Form action="https://www.paypal.com/donate" method="post" target="_top" className="d-inline">
@@ -27,7 +47,7 @@ const DonationsModal = ({ showDonationModal, setShowDonationModal }) => {
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
           <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </Form>
-        <Button variant="secondary" onClick={() => setShowDonationModal(false)}>
+        <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
       </Modal.Footer>
