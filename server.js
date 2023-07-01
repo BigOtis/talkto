a65a257ce07d7a87f8cdf475244fe4593fc44228
Buster <otis1017@gmail.com>
2023-07-01 13:25:12 -0400

    security

--- server.js
+++ server.js
@@ -8,9 +8,35 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+// Middleware to validate request
+const validateRequest = (req, res, next) => {
+    const expectedOrigins = ['chat.otisfuse.com', 'www.otisfuse.com', 'localhost:3000'];
+  
+    // Check the request headers for common properties that indicate a real user/browser
+    const userAgent = req.get('User-Agent');
+    const referer = req.get('Referer');
+    const origin = req.get('Origin');
+  
+    // Perform additional checks as needed
+    if (
+      userAgent &&
+      referer &&
+      origin &&
+      expectedOrigins.some((expectedOrigin) =>
+        origin.toLowerCase().includes(expectedOrigin.toLowerCase())
+      )
+    ) {
+      // Request is coming from a real user/browser and an expected origin
+      next();
+    } else {
+      // Request is suspicious or missing required headers
+      res.status(403).json({ error: 'Invalid request' });
+    }
+  };  
+  
 // router for the api layer
 app.use(bodyParser.json());
-app.use('/api', router);
+app.use('/api', validateRequest, router);
 
 // router for the static files
 app.use(express.static(path.join(__dirname, "build"), { maxAge: "10s"}));
