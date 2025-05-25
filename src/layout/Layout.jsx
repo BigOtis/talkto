877ecf7efdade2fad98b9cc8d05348fc25744291
Buster <otis1017@gmail.com>
2025-05-25 10:37:11 -0400

    Remove footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,13 @@
 // components
 import Menu from "../components/Menu";
-import Footer from "../components/Footer.jsx";
+// import Footer from "../components/Footer.jsx";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Menu />
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
